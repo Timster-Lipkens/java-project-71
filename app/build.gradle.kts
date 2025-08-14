2cfb59708444aaf866ec34e86b2c5568990af8a8
Timster Lipkens <tim.lipkens@gmail.com>
2025-08-14 23:48:25 +0300

    Tests and linters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,22 @@ plugins {
     application
     id("java")
     id("com.github.ben-manes.versions") version "0.39.0"
+
+    checkstyle //всё равно не работает от перемены места
+    id("org.sonarqube") version "6.2.0.5505"
+    jacoco //нужно ещё что-то?
 }
 
 application {
     mainClass = "hexlet.code.App"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Timster-Lipkens_java-project-71")
+        property("sonar.organization", "timster-lipkens")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 group = "hexlet.code"
@@ -20,10 +32,15 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     implementation("info.picocli:picocli:4.7.7")
-
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.11.0-M2")
 }
 
 tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.named<Test>("test") { //нужно или избыточно?
     useJUnitPlatform()
 }
--- app/src/main/java/hexlet/code/Host.java
+++
@@ -1,9 +0,0 @@
-package hexlet.code;
-
-public class Host {
-    public String host;
-    public String proxy;
-    public int timeout;
-    public boolean follow;
-    public boolean verbose;
-}
