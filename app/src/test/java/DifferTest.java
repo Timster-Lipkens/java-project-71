d8d6bea5247681c53fef080e401dbc997a63449f
Timster Lipkens <tim.lipkens@gmail.com>
2025-08-29 06:55:17 +0300

    BeforeAll

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -1,6 +1,7 @@
 import hexlet.code.Differ;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.file.Files;
@@ -8,67 +9,70 @@ import java.nio.file.Paths;
 
 public class DifferTest {
 
-    //тесты умолчания
-    @Test
+    private static String resultDefault; //для зоны видимости
+    private static String resultStylish;
+    private static String resultPlain;
+    private static String resultJson;
+
+    @BeforeAll
+    public static void expected() throws Exception { // немного уменьшает дублирование
+        resultDefault = Files.readString(Paths.get("src/test/resources/file0"));
+        resultStylish = Files.readString(Paths.get("src/test/resources/file12"));
+        resultPlain = Files.readString(Paths.get("src/test/resources/plain"));
+        resultJson = Files.readString(Paths.get("src/test/resources/file12.json"));
+    }
+
+    @Test //тесты умолчания
     public void defaultJson0() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/fileI.json",
                 "src/main/java/hexlet/code/files/fileII.json");
-        var expected = Files.readString(Paths.get("src/test/resources/file0"));
-        assertEquals(expected, actual);
+        assertEquals(resultDefault, actual);
     }
     @Test
     public void defaultYaml0() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/fileI.yaml",
                 "src/main/java/hexlet/code/files/fileII.yaml");
-        var expected = Files.readString(Paths.get("src/test/resources/file0"));
-        assertEquals(expected, actual);
+        assertEquals(resultDefault, actual);
     }
 
-    //рекурсивные тесты
-    @Test
+
+    @Test //рекурсивные тесты
     public void stylishJson12() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/file1.json",
                 "src/main/java/hexlet/code/files/file2.json", "stylish");
-        var expected = Files.readString(Paths.get("src/test/resources/file12"));
-        assertEquals(expected, actual);
+        assertEquals(resultStylish, actual);
     }
     @Test
     public void stylishYaml12() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/file1.yaml",
                 "src/main/java/hexlet/code/files/file2.yaml", "stylish");
-        var expected = Files.readString(Paths.get("src/test/resources/file12"));
-        assertEquals(expected, actual);
+        assertEquals(resultStylish, actual);
     }
 
-    //форматные тесты
-    @Test
+    @Test //форматные тесты
     public void plainJson12() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/file1.json",
                 "src/main/java/hexlet/code/files/file2.json", "plain");
-        var expected = Files.readString(Paths.get("src/test/resources/plain"));
-        assertEquals(expected, actual);
+        assertEquals(resultPlain, actual);
     }
     @Test
     public void plainYaml12() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/file1.yaml",
                 "src/main/java/hexlet/code/files/file2.yaml", "plain");
-        var expected = Files.readString(Paths.get("src/test/resources/plain"));
-        assertEquals(expected, actual);
+        assertEquals(resultPlain, actual);
     }
 
     @Test
     public void jsonJson12() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/file1.json",
                 "src/main/java/hexlet/code/files/file2.json", "json"); // как это работало с yaml?
-        var expected = Files.readString(Paths.get("src/test/resources/file12.json"));
-        assertEquals(expected, actual);
+        assertEquals(resultJson, actual);
     }
     @Test
     public void jsonYaml12() throws Exception {
         var actual = Differ.generate("src/main/java/hexlet/code/files/file1.yaml",
                 "src/main/java/hexlet/code/files/file2.yaml", "json");
-        var expected = Files.readString(Paths.get("src/test/resources/file12.json"));
-        assertEquals(expected, actual);
+        assertEquals(resultJson, actual);
     }
 
 }
